35fa1ea558c87770ba595083ad4bbe5bd4708b8e
Ayachi <97498357+jharambha@users.noreply.github.com>
2023-02-21 00:02:47 +0530

    Add files via upload

---
+++ reactPractice/index.js
@@ -0,0 +1,6 @@
+/*const h1 = document.createElement("h1");
+h1.textContent = "Imperative way";
+h1.className = "header"
+document.getElementById("root").append("kfbdksvknvre", h1);*/
+
+ReactDOM.render(<p className="hx">jhgjhshjgehdvhsbdjhshvxscxsbnvxnbsvxnbsv</p>,document.getElementById("root"))
